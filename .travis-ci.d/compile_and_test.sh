671b94cb6b553e908ce7afa09a4b40cda902378d
Simon Spannagel <simon.spannagel@cern.ch>
2018-05-07 17:26:45 +0200

    Travis CI: force color in Ninja

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -4,7 +4,7 @@ mkdir build
 cd build
 source ../.travis-ci.d/init_x86_64.sh
 source $ROOTSYS/bin/thisroot.sh
-cmake -GNinja -DROOT_DIR=$ROOTSYS -DEigen3_DIR=$Eigen3_DIR .. && \
+cmake -GNinja -DROOT_DIR=$ROOTSYS -DEigen3_DIR=$Eigen3_DIR -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
 ninja && \
 ninja doc && \
 ninja install
