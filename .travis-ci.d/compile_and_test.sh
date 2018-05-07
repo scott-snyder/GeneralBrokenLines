c80887492e21a42d11b59a74150edf80ee8e9586
Simon Spannagel <simon.spannagel@cern.ch>
2018-05-07 14:53:42 +0200

    Try to make Travis CI work

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -2,7 +2,7 @@
 cd /Package
 mkdir build
 cd build
-source init_x86_64.sh
+source ./init_x86_64.sh
 source $ROOTSYS/bin/thisroot.sh
 cmake -GNinja -DROOT_DIR=$ROOTSYS -DEigen3_DIR=$Eigen3_DIR .. && \
 ninja && \
