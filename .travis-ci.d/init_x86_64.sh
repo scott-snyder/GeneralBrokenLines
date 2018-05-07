fe9ffb9009729191576963ebbb994025ae4ec972
Simon Spannagel <simon.spannagel@cern.ch>
2018-05-07 15:09:27 +0200

    Update CI dependencies

--- .travis-ci.d/init_x86_64.sh
+++ .travis-ci.d/init_x86_64.sh
@@ -39,11 +39,11 @@ if [ -z ${COMPILER_TYPE} ]; then
 fi
 if [ ${COMPILER_TYPE} == "gcc" ]; then
     echo "Compiler type set to GCC."
-    COMPILER_VERSION="gcc7"
+    COMPILER_VERSION="gcc8"
 fi
 if [ ${COMPILER_TYPE} == "llvm" ]; then
     echo "Compiler type set to LLVM."
-    COMPILER_VERSION="llvm40"
+    COMPILER_VERSION="llvm6"
 fi
 
 # Choose build type
@@ -61,24 +61,24 @@ export BUILD_FLAVOUR=x86_64-${OS}-${COMPILER_VERSION}-${BUILD_TYPE}
 #--------------------------------------------------------------------------------
 
 if [ ${COMPILER_TYPE} == "gcc" ]; then
-    source ${CLICREPO}/compilers/gcc/7.1.0/x86_64-${OS}/setup.sh
+    source ${CLICREPO}/compilers/gcc/8.1.0/x86_64-${OS}/setup.sh
 fi
 if [ ${COMPILER_TYPE} == "llvm" ]; then
-    source ${CLICREPO}/compilers/llvm/4.0.0/x86_64-${OS}/setup.sh
+    source ${CLICREPO}/compilers/llvm/6.0.0/x86_64-${OS}/setup.sh
 fi
 
 #--------------------------------------------------------------------------------
 #     CMake
 #--------------------------------------------------------------------------------
 
-export CMAKE_HOME=${CLICREPO}/software/CMake/3.8.1/${BUILD_FLAVOUR}
+export CMAKE_HOME=${CLICREPO}/software/CMake/3.11.1/${BUILD_FLAVOUR}
 export PATH=${CMAKE_HOME}/bin:$PATH
 
 #--------------------------------------------------------------------------------
 #     ROOT
 #--------------------------------------------------------------------------------
 
-export ROOTSYS=${CLICREPO}/software/ROOT/6.08.06/${BUILD_FLAVOUR}
+export ROOTSYS=${CLICREPO}/software/ROOT/6.12.06/${BUILD_FLAVOUR}
 export PYTHONPATH="$ROOTSYS/lib:$PYTHONPATH"
 export PATH="$ROOTSYS/bin:$PATH"
 export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
@@ -88,14 +88,14 @@ export CMAKE_PREFIX_PATH="$ROOTSYS:$CMAKE_PREFIX_PATH"
 #     Ninja
 #--------------------------------------------------------------------------------
 
-export Ninja_HOME=${CLICREPO}/software/Ninja/1.7.2/${BUILD_FLAVOUR}
+export Ninja_HOME=${CLICREPO}/software/Ninja/1.8.2/${BUILD_FLAVOUR}
 export PATH="$Ninja_HOME:$PATH"
 
 #--------------------------------------------------------------------------------
 #     Eigen
 #--------------------------------------------------------------------------------
 
-export Eigen_HOME=${CLICREPO}/software/Eigen/3.3.3/${BUILD_FLAVOUR}/
+export Eigen_HOME=${CLICREPO}/software/Eigen/3.3.4/${BUILD_FLAVOUR}/
 export Eigen3_DIR=${Eigen_HOME}/share/eigen3/cmake/
 export CMAKE_PREFIX_PATH="$Eigen3_DIR:$CMAKE_PREFIX_PATH"
 
