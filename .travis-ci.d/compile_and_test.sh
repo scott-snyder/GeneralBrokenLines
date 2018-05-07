e9b6917fe08c65bb42c63c73649ae4f87a2d7ae2
Simon Spannagel <simon.spannagel@cern.ch>
2018-05-07 14:56:55 +0200

    Thanks @perticm for educating me. :)

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -2,7 +2,7 @@
 cd /Package
 mkdir build
 cd build
-source ./init_x86_64.sh
+source ../.travis-ci.d/init_x86_64.sh
 source $ROOTSYS/bin/thisroot.sh
 cmake -GNinja -DROOT_DIR=$ROOTSYS -DEigen3_DIR=$Eigen3_DIR .. && \
 ninja && \
