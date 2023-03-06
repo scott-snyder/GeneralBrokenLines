f35c34a5e983bdcb63285a2761c2b6b1b476e7dc
tomeichlersmith <eichl008@umn.edu>
2023-03-06 15:12:36 -0600

    update tabbing to conform with rest of cpp source

--- cpp/src/MilleBinaryWrapper.cpp
+++ cpp/src/MilleBinaryWrapper.cpp
@@ -4,16 +4,16 @@
 using namespace gbl;
 
 extern "C" {
-    
-    MilleBinary* MilleBinaryCtor(const char* fileName, int filenamesize, int doublePrecision, int aSize) {
-        
-        std::string binName(fileName,filenamesize);
-        
-        return new MilleBinary(binName,doublePrecision,aSize);
-    }
-    
-    void MilleBinary_close(MilleBinary* self) {
-        
-        self->Close();
-    }
+
+MilleBinary* MilleBinaryCtor(const char* fileName, int filenamesize, int doublePrecision, int aSize) {
+	
+	std::string binName(fileName,filenamesize);
+	
+	return new MilleBinary(binName,doublePrecision,aSize);
+}
+
+void MilleBinary_close(MilleBinary* self) {
+	
+	self->Close();
+}
 }
