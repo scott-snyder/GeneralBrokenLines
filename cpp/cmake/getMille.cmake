557a04237f121f5d444f7d8b69882d75b00ad5cb
Maximilian Goblirsch-Kolb <goblirsc@cern.ch>
2026-01-22 12:08:31 +0100

    set Mille download target to tagged version

--- cpp/cmake/getMille.cmake
+++ cpp/cmake/getMille.cmake
@@ -2,14 +2,13 @@
 
 include(FetchContent)
 
-set(MILLE_TARGET_VERSION main) 
 # Flag to adapt config file creation later on 
 set(MILLE_AS_SUBDIR True) 
 
+set(Recommended_Mille_Version V00-01-00)
 
 FetchContent_Declare(Mille 
-    GIT_REPOSITORY https://gitlab.desy.de/millepede/mille.git
-    GIT_TAG ${MILLE_TARGET_VERSION}
+    URL https://gitlab.desy.de/millepede/mille/-/archive/${Recommended_Mille_Version}/mille-${Recommended_Mille_Version}.tar.gz
 )
 
 FetchContent_GetProperties(Mille)
