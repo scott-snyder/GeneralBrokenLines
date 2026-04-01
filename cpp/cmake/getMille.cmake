96b9e25498e298e610213bee4990d3e7216a6389
Maximilian Goblirsch-Kolb <goblirsc@cern.ch>
2026-04-01 12:23:33 +0200

    remove deprecated pattern in Mille installation

--- cpp/cmake/getMille.cmake
+++ cpp/cmake/getMille.cmake
@@ -10,11 +10,9 @@ set(Recommended_Mille_Version V01-00-02)
 FetchContent_Declare(Mille 
     URL https://gitlab.desy.de/millepede/mille/-/archive/${Recommended_Mille_Version}/mille-${Recommended_Mille_Version}.tar.gz
     DOWNLOAD_EXTRACT_TIMESTAMP True
+    SOURCE_SUBDIR DummyForCMake
 )
 
-FetchContent_GetProperties(Mille)
-if(NOT Mille_POPULATED)
-  FetchContent_Populate(Mille)
-  add_subdirectory(${mille_SOURCE_DIR} ${mille_BINARY_DIR})
-  set(Mille_MODULE_DIRS ${mille_BINARY_DIR}/modules)
-endif()
+FetchContent_MakeAvailable(Mille)
+add_subdirectory(${mille_SOURCE_DIR} ${mille_BINARY_DIR})
+set(Mille_MODULE_DIRS ${mille_BINARY_DIR}/modules)
